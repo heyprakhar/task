d473c600dab0f8e8d8fe3950375291dd817a8c21
Prakhar Tripathi <talk2prakhartripathi@gmail.com>
2026-03-07 14:16:44 +0530

    Initial commit

---
+++ src/main/java/com/taskflow/taskk/TaskkApplication.java
@@ -0,0 +1,13 @@
+package com.taskflow.taskk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskkApplication.class, args);
+	}
+
+}
