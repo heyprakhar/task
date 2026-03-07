6887f218442fcac63d84f4ecbfeda81534fe4fa8
Prakhar Tripathi <talk2prakhartripathi@gmail.com>
2026-03-07 14:16:44 +0530

    Initial commit

---
+++ src/test/java/com/taskflow/taskk/TaskkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskflow.taskk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
