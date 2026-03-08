81d6c659c673426fb714cf818acbb54f735e1656
Prakhar Tripathi <talk2prakhartripathi@gmail.com>
2026-03-08 12:58:55 +0530

    Feat: created an api to fetch all users

--- src/main/java/com/taskflow/taskk/service/serviceInterface/UserService.java
+++ src/main/java/com/taskflow/taskk/service/serviceInterface/UserService.java
@@ -1,11 +1,13 @@
 package com.taskflow.taskk.service.serviceInterface;
 
 import org.hibernate.internal.build.AllowPrintStacktrace;
+import java.util.List;
 
 // import statements -
 import com.taskflow.taskk.dto.requestDto.UserRequestDto;
 import com.taskflow.taskk.dto.responseDto.UserResponseDto;
 
 public interface UserService {
-UserResponseDto createUser(UserRequestDto userRequestDto);
+    UserResponseDto createUser(UserRequestDto userRequestDto);
+        List<UserResponseDto> fetchAllUsers();
 }
