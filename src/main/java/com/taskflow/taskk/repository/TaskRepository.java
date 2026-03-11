c5bc63d612924ae34048f031502045e15400e311
Prakhar Tripathi <talk2prakhartripathi@gmail.com>
2026-03-11 22:20:21 +0530

    Impv: fetched tasks with filters(status,priority)

--- src/main/java/com/taskflow/taskk/repository/TaskRepository.java
+++ src/main/java/com/taskflow/taskk/repository/TaskRepository.java
@@ -6,9 +6,16 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.taskflow.taskk.entity.Task;
-import java.util.Optional;
+import com.taskflow.taskk.enums.TaskStatus;
+import com.taskflow.taskk.enums.TaskPriority;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, UUID> {
      List<Task> findByAssignedToId(UUID userId);
+
+     List<Task> findByStatusAndPriority(TaskStatus status, TaskPriority priority);
+     
+     List<Task> findByStatus(TaskStatus status);
+
+     List<Task> findByPriority(TaskPriority priority);
 }
