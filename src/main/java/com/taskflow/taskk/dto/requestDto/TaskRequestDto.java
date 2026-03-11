5652abf0c681d334cdd513247c98896e8eea83d2
Prakhar Tripathi <talk2prakhartripathi@gmail.com>
2026-03-11 19:48:12 +0530

    feat: added api to update task status

--- src/main/java/com/taskflow/taskk/dto/requestDto/TaskRequestDto.java
+++ src/main/java/com/taskflow/taskk/dto/requestDto/TaskRequestDto.java
@@ -2,14 +2,10 @@ package com.taskflow.taskk.dto.requestDto;
 
 import lombok.Data;
 import com.taskflow.taskk.enums.TaskPriority;
-import com.taskflow.taskk.enums.TaskStatus;
-import java.util.UUID;
 
 @Data
 public class TaskRequestDto {
     private String title;
     private String description;
-    private TaskStatus status;
     private TaskPriority priority;
-    private UUID assignedTo;
 }
