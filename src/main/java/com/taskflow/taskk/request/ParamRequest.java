03f8e686c67125af3637cb7b4f1431f0aa81e682
Prakhar Tripathi <talk2prakhartripathi@gmail.com>
2026-07-25 16:49:57 +0530

    task: created a centralised listing dto

---
+++ src/main/java/com/taskflow/taskk/request/ParamRequest.java
@@ -0,0 +1,25 @@
+package com.taskflow.taskk.request;
+
+import com.taskflow.taskk.dto.requestDto.ListingRequestDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import java.util.List;
+
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder(toBuilder = true)
+public class ParamRequest extends ListingRequestDto{
+
+    private List<String> entityState;
+    private List<String> entityType;
+    private Boolean records = Boolean.FALSE;
+
+    public boolean isRecords() { return Boolean.TRUE.equals(records); }
+
+}
